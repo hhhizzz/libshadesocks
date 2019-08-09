bbc7ac86523dd4703f9b8787b3af41b96afd7b81
xunix huang <qiwei_huang@qq.com>
2019-08-09 21:19:37 +0800

    modify dependency

--- src/ss_test.cc
+++ src/ss_test.cc
@@ -1,12 +1,15 @@
 #include <gtest/gtest.h>
+#include <glog/logging.h>
 #include "ss.h"
 
 TEST(FooTest, HandleNoneZeroInput) {
+  LOG(ERROR)<<"start to test";
   EXPECT_EQ(2, Foo(4, 10));
   EXPECT_EQ(6, Foo(30, 18));
 }
 
 int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
